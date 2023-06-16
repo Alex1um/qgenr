d492b6b74a098c8270f2def9dc844c15b685a208
Alex1um <assassin20563@gmail.com>
2023-06-17 01:12:48 +0700

    added border button to bot

--- bot.py
+++ bot.py
@@ -12,6 +12,39 @@ def create_bot():
 
     dp = Dispatcher(bot)
 
+    @dp.callback_query_handler()
+    async def bt_handler(cq: types.callback_query.CallbackQuery):
+        message = cq.message
+        param, new_value = cq.data.split("=")
+        if param == "none":
+            return
+        command = message.reply_to_message.get_command(True)
+        kb = message.reply_markup
+        bts = kb['inline_keyboard']
+        match param:
+            case 'invert':
+                bt = bts[0][0]
+                bt.callback_data = f"invert={'' if new_value else 1}"
+            case 'border':
+                bt = bts[1]
+                new_value = int(new_value)
+                bt[0].callback_data = f"border={new_value - 1 if new_value >= 0 else 0}"
+                bt[1].text = f"border={new_value}"
+                bt[2].callback_data = f"border={new_value + 1}"
+        params = {
+            "border": bts[1][1].text.split("=")[1],
+            "invert": bool(bts[0][0].callback_data.split("=")[1])
+        }
+
+        if command == "qr":
+            code = get_bytes(message.reply_to_message.get_args(), **params)
+
+            await message.edit_media(types.InputMediaPhoto(types.InputFile(code)), reply_markup=kb)
+        elif command == "ascii":
+            code = get_ascii_qr(message.reply_to_message.get_args(), **params)
+
+            await message.edit_text(f"```\n{code}```", parse_mode="markdown", reply_markup=kb)
+
     @dp.message_handler(commands=["qr"], regexp=r"^(\/qr)(\s\S+)+(\s+)?$")
     async def on_qr(message: Message) -> None:
         payload = message.get_args()
@@ -21,28 +54,13 @@ def create_bot():
 
             kb = types.InlineKeyboardMarkup()
             kb.add(types.InlineKeyboardButton("invert", callback_data="invert=1"))
+            kb.add(
+                types.InlineKeyboardButton("-", callback_data="border=3"),
+                types.InlineKeyboardButton("border: 0", callback_data="none"),
+                types.InlineKeyboardButton("+", callback_data="border=5"),
+            )
 
             await message.reply_photo(types.InputFile(bytes_img), caption=payload, reply_markup=kb)
-
-    @dp.callback_query_handler()
-    async def bt_handler(cq: types.callback_query.CallbackQuery):
-        message = cq.message
-        param, new_value = cq.data.split("=")
-        command = message.reply_to_message.get_command(True)
-        if command == "qr":
-            code = get_bytes(message.reply_to_message.get_args(), **{param: new_value})
-
-            kb = types.InlineKeyboardMarkup()
-            kb.add(types.InlineKeyboardButton("invert", callback_data=f"invert={'' if new_value else 1}"))
-
-            await message.edit_media(types.InputMediaPhoto(types.InputFile(code)), kb)
-        elif command == "ascii":
-            code = get_ascii_qr(message.reply_to_message.get_args(), **{param: new_value})
-
-            kb = types.InlineKeyboardMarkup()
-            kb.add(types.InlineKeyboardButton("invert", callback_data=f"invert={'' if new_value else 1}"))
-
-            await message.edit_text(f"```\n{code}```", parse_mode="markdown", reply_markup=kb)
 
     @dp.message_handler(commands=["ascii"], regexp=r"^(\/ascii)(\s\S+)+(\s+)?$")
     async def on_ascii(message: Message) -> None:
@@ -53,6 +71,11 @@ def create_bot():
 
             kb = types.InlineKeyboardMarkup()
             kb.add(types.InlineKeyboardButton("invert", callback_data="invert=1"))
+            kb.add(
+                types.InlineKeyboardButton("-", callback_data="border=0"),
+                types.InlineKeyboardButton("border=0", callback_data="none"),
+                types.InlineKeyboardButton("+", callback_data="border=1"),
+            )
 
             await message.reply(f"```\n{code}```", parse_mode="markdown", reply_markup=kb)
 
